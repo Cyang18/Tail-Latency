0ec7d4b613497f933b2b7cdea6b0f9f2fa03504a
Christopher yang <146152532+Cyang18@users.noreply.github.com>
2023-11-09 02:15:47 -0900

    Update main.py

--- SJF_Code/main.py
+++ SJF_Code/main.py
@@ -1,30 +1,19 @@
-#Version 0.1: This is prototype that simulates the shortest job first.
-def shortest_job_first(packages):
-    current_time = 0
-    schedule = []
+from mininet.topo import Topo
 
-    # Sort the jobs by burst time (the time required to complete each job)
-    # Assuming (job_id, burst_time)
-    packages.sort(key=lambda x: x[1])
+class MyTopo( Topo ):
+    "Simple topology example."
 
-    #iterate through the array
-    for job_id, burst_time in packages:
+    def build( self ):
+        "Create custom topo."
 
-        # re-schedule the schedule after being sorted
-        schedule.append((job_id, current_time))
+        # Add hosts and switches
+        h1 = self.addHost( 'h1' )
+        h2 = self.addHost( 'h2' )
+        s1 = self.addSwitch( 's1' )
 
-        # Update the current time and subtract the job's burst time
-        # Gets the time frame of when each package is gonna be sent
-        current_time += burst_time
-
-    return schedule
+        # Add links
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
 
 
- # List of jobs, each represented as (job_id, burst_time)
-packages = [(1, 6), (4, 3), (5, 2)]
-schedule = shortest_job_first(packages)
-
-print("Schedule of each Job being processed (Job ID, Start Time):")
-for job_id, start_time in schedule:
-    print(f"Job_ID {job_id}: Wait time {start_time}")
-
+topos = { 'mytopo': ( lambda: MyTopo() ) }
