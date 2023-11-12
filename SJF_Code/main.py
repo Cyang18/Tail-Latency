8ac8842194010cb2dd83f1ea415ba93961fd8ce9
Christopher yang <146152532+Cyang18@users.noreply.github.com>
2023-11-11 15:50:07 -0900

    Update main.py

--- SJF_Code/main.py
+++ SJF_Code/main.py
@@ -2,6 +2,7 @@ from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import Host, OVSKernelSwitch
 from mininet.link import TCLink
+import time
 
 class MyTopo( Topo ):
 
@@ -24,10 +25,32 @@ def run_sjf():
     net.start()
     h1, h2, h3 = net.get('h1', 'h2', 'h3')
 
+
+    s_sent = time.time()    #temp for recording the time when a packet is sent
     h1.cmd('echo "Job 1 started"')
+    e_end = time.time()    #temp for recording time when packet recieved 
+
+    s_sent1 = time.time()    #temp for recording the time when a packet is sent
     h2.cmd('echo "Job 2 started"')
+    e_end1 = time.time()    #temp for recording time when packet recieved 
+
+    s_sent2 = time.time()    #temp for recording the time when a packet is sent
     h3.cmd('echo "Job 3 started"')
+    e_end2 = time.time()    #temp for recording time when packet recieved 
+
+
+    #  so this portion is to calcuate the completion time
+    #  so that i can figure out the tail-latency
+    total_time = e_end - s_sent
+    total_time1 = e_end1 - s_sent1
+    total_time2 = e_end2 - s_sent2
+
     
+    print("Completion time for h1: {} seconds".format(total_time))
+    print("Completion time for h2: {} seconds".format(total_time1))
+    print("Completion time for h3: {} seconds".format(total_time2))
+    
+
     net.stop()
 
 if __name__ == '__main__':
